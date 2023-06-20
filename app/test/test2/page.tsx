3e2e9b237425ece5096695371d35d5640ab2618d
ErikDale <erikdale@stud.ntnu.no>
2023-06-20 08:41:45 +0200

    Made changes

---
+++ app/test/test2/page.tsx
@@ -0,0 +1,9 @@
+const TestTest = () => {
+    return (
+        <div>
+            <h1 className="bg-orange-400 text-black font-sans">Test Test</h1>
+        </div>
+    )
+}
+
+export default TestTest;
